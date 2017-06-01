43dc7b8a44ebe344ad0e7b51ac0fb62d83b6e931
Shia <rise.shia@gmail.com>
2017-06-01 09:20:33 +0900

    Keep sign in
    close #23

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,7 @@ class User < ApplicationRecord
       user.uid = auth.uid
       user.email = auth.info.email
       user.password = Devise.friendly_token[0, 20]
+      user.remember_me = true
     end
   end
 end
